9708cd640b50d9ea33a048b7bed2b8db3d976a51
Abduraimbek Yarkinov <yarkinovabduraimbek@gmail.com>
2023-10-13 09:19:07 +0300

    delete user

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 
-import { getUsers } from '../db/userModel';
+import { deleteUserById, getUsers } from '../db/userModel';
 
 export const getAllUsers = async (
   req: express.Request,
@@ -10,6 +10,22 @@ export const getAllUsers = async (
     const users = await getUsers();
 
     return res.status(200).json(users).end();
+  } catch (err) {
+    console.log(err);
+    return res.sendStatus(400);
+  }
+};
+
+export const deleteUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const { id } = req.params;
+
+    const deletedUser = await deleteUserById(id);
+
+    return res.json(deletedUser).end();
   } catch (err) {
     console.log(err);
     return res.sendStatus(400);
